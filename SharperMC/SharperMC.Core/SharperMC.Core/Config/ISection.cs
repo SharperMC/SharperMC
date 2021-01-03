76521f43463458d01eff500af2f14211a7ca9df2
true-cc <creepercannyon@gmail.com>
2021-01-02 17:41:03 -0800

    Implemented ToObject for ISection.cs but didn't test yet.

--- SharperMC/SharperMC.Core/SharperMC.Core/Config/ISection.cs
+++ SharperMC/SharperMC.Core/SharperMC.Core/Config/ISection.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -18,9 +19,9 @@ namespace SharperMC.Core.Config
         /// <param name="defaultValue">The value to return if not found.</param>
         /// <returns>The value or defaultValue if not found</returns>
         public object Get(string key, object defaultValue = null);
-        
+
         public IDictionary<string, object> GetKeys();
-        
+
         public virtual string GetString(string key, string defaultValue = "")
         {
             return Get(key, defaultValue) as string ?? defaultValue;
@@ -90,7 +91,7 @@ namespace SharperMC.Core.Config
         /// <param name="value">The value itself.</param>
         /// <exception cref="System.ArgumentException">If a key is found to not be a section.</exception>
         public void Set(string key, object value);
-        
+
         /// <summary>
         /// Generates a new ISection.
         /// </summary>
@@ -98,5 +99,35 @@ namespace SharperMC.Core.Config
         /// <param name="dict">The section's dict.</param>
         /// <returns>A new ISection</returns>
         public ISection NewSection(string key, Dictionary<string, object> dict);
+
+        /// <summary>
+        /// For each field in type, check if this section's keys contains the fields name. If so, set the field's
+        /// value to the section's value (from key) with the given obj.
+        /// </summary>
+        /// <param name="type">The type to get keys from</param>
+        /// <param name="obj">Set null if it's static</param>
+        public void ToObject(Type type, object obj)
+        {
+            var keys = GetKeys();
+            foreach (var field in type.GetFields())
+                if (keys.ContainsKey(field.Name))
+                    field.SetValue(obj, keys[field.Name]);
+        }
+
+        /// <summary>
+        /// Same as ToObject(Type type, object obj) but limits to certain values.
+        /// </summary>
+        /// <param name="type">The type to get keys from</param>
+        /// <param name="obj">Set null if it's static</param>
+        /// <param name="values">The limited values</param>
+        public void ToObject(Type type, object obj, IEnumerable<string> values)
+        {
+            var k = GetKeys();
+            var keys = GetKeys().Keys.Intersect(values);
+            var enumerable = keys as string[] ?? keys.ToArray();
+            foreach (var field in type.GetFields())
+                if (enumerable.Contains(field.Name))
+                    field.SetValue(obj, k[field.Name]);
+        }
     }
 }
