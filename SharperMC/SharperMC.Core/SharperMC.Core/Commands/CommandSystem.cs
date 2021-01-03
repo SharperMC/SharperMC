07de0213ad372420f3370ca5bac3aca82bdf9e86
true-cc <creepercannyon@gmail.com>
2021-01-03 11:33:10 -0800

    Implemented CommandSystem#ParseCommand and CommandSystem#ParseTab

--- SharperMC/SharperMC.Core/SharperMC.Core/Commands/CommandSystem.cs
+++ SharperMC/SharperMC.Core/SharperMC.Core/Commands/CommandSystem.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using SharperMC.Core.Utils.Console;
+using SharperMC.Core.Utils.Text;
 
 namespace SharperMC.Core.Commands
 {
@@ -13,32 +16,97 @@ namespace SharperMC.Core.Commands
             Prefixes = prefixes;
         }
         
-        public IEnumerable<string> GetPrefixes()
+        public virtual IEnumerable<string> GetPrefixes()
         {
             return Prefixes;
         }
 
-        public void ParseCommand(ICommandSender sender, string message)
+        public virtual void ParseCommand(ICommandSender sender, string message)
         {
-            throw new System.NotImplementedException();
+            var origMessage = message;
+            try
+            {
+                message = message.Trim();
+                while (message.Contains("  ")) message = message.Replace("  ", " ");
+                var split = message.Split(' ');
+                var command = GetCommand(split[0]);
+                if (command == default(Command))
+                {
+                    UnknownCommand(sender, split[0]);
+                    return;
+                }
+
+                string[] args;
+                if (split.Length > 1)
+                {
+                    args = new string[split.Length - 1];
+                    Array.Copy(split, 1, args, 0, split.Length - 1);
+                }
+                else args = new string[0];
+
+                command.Execute(sender, split[0], args, origMessage);
+            }
+            catch (Exception ex)
+            {
+                ConsoleFunctions.WriteWarningLine(ex.ToString());
+                sender.SendChat("An error occured when executing this command.", TextColor.Red);
+            }
         }
 
-        public IEnumerable<string> ParseTab(ICommandSender sender, string message)
+        public virtual IEnumerable<string> ParseTab(ICommandSender sender, string message)
         {
-            throw new System.NotImplementedException();
+            if (string.IsNullOrEmpty(message))
+            {
+                var strings = new List<string>();
+                foreach (var keyValuePair in CommandMap)
+                {
+                    strings.AddRange(keyValuePair.Key);
+                }
+                return strings;
+            }
+            var origMessage = message;
+            try
+            {
+                message = message.Trim();
+                while (message.Contains("  ")) message = message.Replace("  ", " ");
+                var split = message.Split(' ');
+                var command = GetCommand(split[0]);
+                if (command == default(Command)) return new List<string>();
+
+                string[] args;
+                if (split.Length > 1)
+                {
+                    args = new string[split.Length - 1];
+                    Array.Copy(split, 1, args, 0, split.Length - 1);
+                }
+                else args = new string[0];
+
+                return command.TabComplete(sender, split[0], args, origMessage).ToList();
+            }
+            catch (Exception ex)
+            {
+                ConsoleFunctions.WriteWarningLine(ex.ToString());
+                sender.SendChat("An error occured when tab-completing the command.", TextColor.Red);
+            }
+            return new List<string>();
         }
 
-        public void AddCommand(Command command)
+        protected virtual void UnknownCommand(ICommandSender sender, string label)
+        {
+            sender.SendChat("Unknown command: " + label, TextColor.Red);
+        }
+
+        public virtual void AddCommand(Command command)
         {
             var aliases = new string[command.Aliases.Length + 1];
-            command.Aliases.CopyTo(aliases, 0);
-            aliases[^1] = command.Name;
+            command.Aliases.CopyTo(aliases, 1);
+            aliases[0] = command.Name;
             for (var i = 0; i < aliases.Length; i++)
                 aliases[i] = aliases[i].ToLower();
             CommandMap.Add(aliases, command);
         }
 
-        public Command GetCommand(string label)
+        public virtual Command GetCommand(string label)
         {
             label = label.ToLower();
             return CommandMap.FirstOrDefault(s => s.Key.Contains(label)).Value;
